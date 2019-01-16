f2ce8e108d12260652007647a7b24590cb19e08b
hsefein <grentorious@gmail.com>
2019-01-16 14:20:54 +0000

    added circular button

---
+++ app.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from '@angular/core';
+import {Project} from './model/project';
+import {ProjectService} from './service/project-service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'connectAngular';
+  projects: Project[] = [];
+
+  constructor(public projectService: ProjectService) {
+  }
+
+  ngOnInit(): void {
+    this.projectService.getAll().subscribe(value => {
+      this.projects = value;
+    });
+}
+
+  addProject(project_name: string) {
+    const project: Project = new Project();
+    project.project_name = project_name;
+    this.projectService.create(project).subscribe(value => {
+      this.ngOnInit();
+    });
+}
+
+  deleteProject(project: Project) {
+    this.projectService.delete(project).subscribe(value => {
+      this.ngOnInit();
+    });
+  }
+
+  updateProject(project: Project) {
+    this.projectService.patch(project).subscribe(value1 => {
+      this.ngOnInit();
+    });
+  }
+
+}
+
+
+
+
